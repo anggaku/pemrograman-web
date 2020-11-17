bf561fd3fba17ac227c2d8d14f40ff98d7f98abe
anggaku <antoniusanggakurniawan@gmail.com>
2020-11-17 16:58:59 +0700

    add M4 and M5

---
+++ M4/js/hello.js
@@ -0,0 +1,7 @@
+// A function to display a message
+function sayHello() {
+    alert("Welcome to Pemrograman Web week four");
+}
+
+// Call function on click of the button
+document.getElementById("myBtn").onclick = sayHello;
